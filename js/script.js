fdbdead800986b04dca5ad77d9b0c4bfe657f622
Matheus Poli Chiarelli <matheusp.chiarelli@gmail.com>
2026-02-23 16:35:05 -0300

    Força a rolagem suave com js

--- js/script.js
+++ js/script.js
@@ -8,3 +8,14 @@ function openTab(evt, tabName) {
     document.getElementById(tabName).classList.add('active');
     evt.currentTarget.classList.add('active');
 }
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        if(target){
+            target.scrollIntoView({ behavior: 'smooth' });
+        }
+    });
+});
